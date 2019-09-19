f5631531067356c9ea3e9eb153d767fa51c93acd
macstudent <mittra.rajdeep@gmail.com>
2019-09-18 21:02:43 -0400

    day_first

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -1,25 +1,50 @@
+
 package com.lambton;
+
+import java.util.Arrays;
 
 public class FirstDay
 {
- public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming");
 
-   System.out.println("welcome to java");
-//Variable declaration
-   System.out.println("Rajdeep");
-//Value Assignment
-   int a,b,c;
-a =10;
-b = 20;
+        //Variable declaration
+        int a, b, c;
 
-c = a+b;
-//Display Result
-System.out.println("SUM:" + c);
+        //Value Assignment
+        a = 10;
+        b = 20;
 
- }
+        c = a + b;
 
-public class student
-{ int studentid;
-string studentName;}
+        //Display Result
+        System.out.println("SUM : " + c);
+
+        int x = Integer.MAX_VALUE;
+        System.out.println("X : " + x);
+
+        int y = x + 1;
+        System.out.println("Y : " + y);
+        System.out.println("Min : " + Integer.MIN_VALUE);
+
+
+
+        student s1 = new student();
+        s1.setValues();
+        s1.display();
+
+        student s2 = new student();
+        s2.studentID = 2;
+        s2.studentName = "RAJDEEEP";
+        s2.display();
+
+        student s3 = new student();
+        s3.studentID = 3;
+        s3.studentName = "RSAJ";
+        s3.display();
+
+
+
+    }
 }
-
--- src/student.java
+++ src/student.java
@@ -1,4 +1,19 @@
-public class student {
+package com.lambton;
+
+public class student
+{
     int studentID;
-    string studentName;
+    String studentName;
+
+    void setValues()
+    {
+        studentID = 1;
+        studentName = "Rajdeep";
     }
+
+    void display()
+    {
+        System.out.println("Student ID   : " + studentID);
+        System.out.println("Student Name : " + studentName);
+    }
+}
